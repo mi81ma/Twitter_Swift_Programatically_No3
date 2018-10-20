39399dcaa787b9825e2eaecffb1e5a61a7c3fddf
masato <masato@m-mbp.local>
2018-10-20 21:28:05 +0800

    make Footer in HomeDatasourceController

--- Twitter_Swift_Programatically_No3/HomeDatasourceController.swift
+++ Twitter_Swift_Programatically_No3/HomeDatasourceController.swift
@@ -8,7 +8,30 @@
 
 import LBTAComponents
 
+// Header
+class UserHeader: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .blue
+    }
+}
+
+
+// Footer
+class UserFooter: DatasourceCell {
+    override func setupViews() {
+        super.setupViews()
+        backgroundColor = .green
+    }
+}
+
 class UserCell: DatasourceCell {
+
+    override var datasourceItem: Any? {
+        didSet {
+            nameLabel.text = datasourceItem as? String
+        }
+    }
 
     let nameLabel: UILabel = {
         let label = UILabel()
@@ -28,6 +51,16 @@ class UserCell: DatasourceCell {
 
 class HomeDatasouce: Datasource {
     let words = ["user1", "user2", "user3"]
+
+    // Header
+    override func headerClasses() -> [DatasourceCell.Type]? {
+        return [UserHeader.self]
+    }
+
+    // Footer
+    override func footerClasses() -> [DatasourceCell.Type]? {
+        return [UserFooter.self]
+    }
 
 
     // DatasourceCellは、UICollectionViewCellを継承している
@@ -55,9 +88,18 @@ class HomeDatasourceController: DatasourceController {
         // HomeDatasourse Initiarize
         let homeDatasource = HomeDatasouce()
 
-        // DatasourceControllerの.datasource objに代入する
+        // DatasourceContrlolerの.datasource objに代入する
         self.datasource = homeDatasource
+    }
 
 
+    // Header Size
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+
+    // Footer Size: FooterをHomeDatasource()で作成してから、RenderをOverRideしている
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
     }
 }
---
+++ Twitter_Swift_Programatically_No3/View/Cells.swift
@@ -0,0 +1,9 @@
+//
+//  Cells.swift
+//  Twitter_Swift_Programatically_No3
+//
+//  Created by masato on 20/10/2018.
+//  Copyright © 2018 masato. All rights reserved.
+//
+
+import Foundation
