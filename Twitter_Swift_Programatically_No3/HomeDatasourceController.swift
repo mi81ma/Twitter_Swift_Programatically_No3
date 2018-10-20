295bc34a0f7f795b42e6efbb8c5c2859ace84973
masato <masato@m-mbp.local>
2018-10-20 19:44:47 +0800

    represent Yellow Cell by DatasourceCell

--- Twitter_Swift_Programatically_No3/HomeDatasourceController.swift
+++ Twitter_Swift_Programatically_No3/HomeDatasourceController.swift
@@ -8,9 +8,45 @@
 
 import LBTAComponents
 
+class UserCell: DatasourceCell {
+    override func setupViews() {
+         super.setupViews()
+        backgroundColor = .yellow
+    }
+}
+
+class HomeDatasouce: Datasource {
+    let words = ["user1", "user2", "user3"]
+
+
+    // DatasourceCellは、UICollectionViewCellを継承している
+    override func cellClasses() -> [DatasourceCell.Type] {
+        return [UserCell.self]
+    }
+
+    // get the Value from array "words"
+    override func item(_ indexPath: IndexPath) -> Any? {
+        return words[indexPath.item]
+    }
+
+    override func numberOfItems(_ section: Int) -> Int {
+        return words.count
+    }
+}
+
+
+
+// HomeDatasourceControllerがwindowに表示される
 class HomeDatasourceController: DatasourceController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .red
+
+        // HomeDatasourse Initiarize
+        let homeDatasource = HomeDatasouce()
+
+        // DatasourceControllerの.datasource objに入れる
+        self.datasource = homeDatasource
+
+
     }
 }
